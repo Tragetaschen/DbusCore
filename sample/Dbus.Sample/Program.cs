54b5c21b76ed6a400112bc64b003319bd0f27713
Kai Ruhnau <kai.ruhnau@target-sg.com>
2017-01-03 12:51:37 +0100

    Don't block the continuation as this locks up reading from the socket

--- sample/Dbus.Sample/Program.cs
+++ sample/Dbus.Sample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Dbus.Sample
@@ -9,18 +10,23 @@ namespace Dbus.Sample
     {
         public static void Main(string[] args)
         {
-            Task.Run(work).Wait();
+            var tcs = new TaskCompletionSource<int>();
+            var task = Task.Run(() => work(tcs.Task));
+            Console.ReadLine();
+            tcs.SetResult(0);
+            task.Wait();
         }
 
-        private static async Task work()
+        private static async Task work(Task shouldContinue)
         {
             Console.WriteLine("Running");
             using (var connection = await Connection.CreateAsync())
             {
                 Console.WriteLine("Connected");
-                var path = await connection.HelloAsync();
+                var orgFreedesktopDbus = new OrgFreedesktopDbus(connection);
+                var path = await orgFreedesktopDbus.HelloAsync();
                 Console.WriteLine($"Done: {path}");
-                Console.ReadLine();
+                await shouldContinue;
             }
         }
     }
