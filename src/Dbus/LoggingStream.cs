c03b5cfb0532146f51882aa1900ffefda7c36e4b
Kai Ruhnau <kai.ruhnau@target-sg.com>
2017-01-04 09:44:39 +0100

    Only print ASCII letters and digits

--- src/Dbus/LoggingStream.cs
+++ src/Dbus/LoggingStream.cs
@@ -58,10 +58,16 @@ namespace Dbus
         private void dump(byte[] buffer, int offset, int length)
         {
             for (var i = offset; i < length; ++i)
-                if (char.IsLetterOrDigit((char)buffer[i]))
+            {
+                var isDigitOrAsciiLetter = false;
+                isDigitOrAsciiLetter |= 48 <= buffer[i] && buffer[i] <= 57;
+                isDigitOrAsciiLetter |= 65 <= buffer[i] && buffer[i] <= 90;
+                isDigitOrAsciiLetter |= 97 <= buffer[i] && buffer[i] <= 122;
+                if (isDigitOrAsciiLetter)
                     Console.Write($"{(char)buffer[i]} ");
                 else
                     Console.Write($"x{buffer[i]:X} ");
+        }
             Console.WriteLine();
         }
 
