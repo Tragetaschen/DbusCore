e239cc9b2b974f9da09d955372bd08c6cd6cb061
Kai Ruhnau <kai.ruhnau@target-sg.com>
2017-01-10 12:25:48 +0100

    Add a ASP.NET core sample application

---
+++ sample/WebApplication.Dbus/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Dbus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new[] {
+                ":1.1",
+                ":1.2",
+                "org.freedesktop.Dbus",
+            });
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
