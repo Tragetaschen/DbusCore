2b39274945ef7015c33581ebb92082893a76f935
Dumitru Suschevici <dsusch@hotmail.com>
2017-06-28 15:00:07 +0200

    Implemented the org.freedesktop.DBus.ObjectManager Interface.
    Since the proxy is not generated, the code-generator has to explicitly add
    a line to register its factory method with the connection.

---
+++ src/Dbus/IOrgFreedesktopDBusObjectManagerProvide.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dbus
+{
+    public interface IOrgFreedesktopDbusObjectManagerProvide
+    {
+        ObjectPath root { get; }
+        Task<Dictionary<ObjectPath, List<IProxy>>> GetManagedObjectsAsync();
+    }
+}
---
+++ src/Dbus/OrgFreedesktopDbusObjectManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dbus
+{
+    public class OrgFreedesktopDbusObjectManager : IOrgFreedesktopDbusObjectManagerProvide
+    {
+
+        public ObjectPath root { get; }
+
+        private readonly Connection connection;
+        private readonly Dictionary<ObjectPath, List<IProxy>> managedObjects;
+        //        private Dictionary<ObjectPath, IDictionary<string, IDictionary<string, object>>> managedObjects;
+
+        public OrgFreedesktopDbusObjectManager(Connection connection, ObjectPath root)
+        {
+            this.connection = connection;
+            this.root = root;
+            managedObjects = new Dictionary<ObjectPath, List<IProxy>>() { };
+        }
+
+        public event Action<ObjectPath, IDictionary<string, IDictionary<string, object>>> InterfacesAdded;
+        public event Action<ObjectPath, IEnumerable<string>> InterfacesRemoved;
+
+        public void AddObject<TInterface, TImplementation>(TImplementation instance, ObjectPath path) where TImplementation : TInterface
+        {
+            string editedPath;
+            if (path.ToString() == "/")
+                editedPath = "";
+            else
+                editedPath = path.ToString();
+            var proxy = (IProxy)connection.Publish<TInterface>(instance, root.ToString() + editedPath);
+            if (managedObjects.ContainsKey(root.ToString() + editedPath))
+            {
+                managedObjects[root.ToString() + editedPath].Add((proxy));
+            }
+            else
+            {
+                managedObjects.Add(root.ToString() + editedPath, new List<IProxy>() { (proxy) });
+            }
+        }
+
+        public async Task<Dictionary<ObjectPath, List<IProxy>>> GetManagedObjectsAsync()
+        {
+            await Task.Delay(1000);
+            return managedObjects;
+        }
+
+        public void Dispose() =>
+            throw new NotImplementedException();
+    }
+}
