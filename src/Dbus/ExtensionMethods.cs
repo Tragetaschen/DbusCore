e44f3140b3effa2e0d3dd22947fe185fbed848ca
Kai Ruhnau <kai.ruhnau@target-sg.com>
2017-09-14 17:11:10 +0200

    There's no way to use the LoggingStream anymore. Reduce it to the one interesting method

---
+++ src/Dbus/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dbus
+{
+    internal static class ExtensionMethods
+    {
+        public static void Dump(this byte[] buffer)
+        {
+            for (var i = 0; i < buffer.Length; ++i)
+            {
+                var isDigitOrAsciiLetter = false;
+                isDigitOrAsciiLetter |= 48 <= buffer[i] && buffer[i] <= 57;
+                isDigitOrAsciiLetter |= 65 <= buffer[i] && buffer[i] <= 90;
+                isDigitOrAsciiLetter |= 97 <= buffer[i] && buffer[i] <= 122;
+                if (isDigitOrAsciiLetter)
+                    Console.Write($"{(char)buffer[i]} ");
+                else
+                    Console.Write($"x{buffer[i]:X} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
