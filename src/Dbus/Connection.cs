ea4b101b80ead562177ccf6f961c9c72e682d262
Kai Ruhnau <kai.ruhnau@deepl.com>
2023-12-22 11:45:44 +0100

    Use the interface when registering the org.freedesktop.dbus consumer

--- src/Dbus/Connection.cs
+++ src/Dbus/Connection.cs
@@ -25,7 +25,7 @@ namespace Dbus
             semaphoreSend = new SemaphoreSlim(1);
             receiveCts = new CancellationTokenSource();
             orgFreedesktopDbus = new OrgFreedesktopDbus(this);
-            consumeFactories.Add(typeof(OrgFreedesktopDbus), (_, _, _, _) => orgFreedesktopDbus);
+            consumeFactories.Add(typeof(IOrgFreedesktopDbus), (_, _, _, _) => orgFreedesktopDbus);
 
             receiveTask = Task.Factory.StartNew(
                 receive,
